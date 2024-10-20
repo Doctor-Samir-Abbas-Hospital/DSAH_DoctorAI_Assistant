ca99d36ddff7b77e8954a1232724718785dd9c35
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2024-10-20 13:53:02 +0300

    translate feature added

--- main.py
+++ main.py
@@ -3,12 +3,40 @@ from streamlit_navigation_bar import st_navbar
 import app
 import translate
 
-# Set page configuration here
-st.set_page_config(page_title="Doctor AI Assistant", page_icon="assets/Dsahicon.png")
+# Set page configuration
+st.set_page_config(
+    page_title="Doctor AI Assistant",
+    page_icon="assets/Dsahicon.png",
+    initial_sidebar_state="collapsed"
+)
 
+# Create the navigation bar with styles
+pages = ["AI Doctor Assistant", "Medical Report Translator"]
+styles = {
+    "nav": {
+        "background-color": "rgb(123, 209, 146)",
+        "fontsize":"0.5rem",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "border-radius": "0.5rem",
+        "color": "rgb(49, 51, 63)",
+        "margin": "0 0.125rem",
+        "padding": "0.4375rem 0.625rem",
+        "fontsize":"0.5rem"
+    },
+    "active": {
+        "background-color": "rgba(255, 255, 255, 0.25)",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
 
-# Create the navigation bar
-page = st_navbar(["AI Doctor Assistant", "Medical Report Translator"])
+# Pass the list and styles directly
+page = st_navbar(pages, styles=styles)
 
 # Conditional rendering based on the selected page
 if page == "AI Doctor Assistant":
