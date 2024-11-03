e93852869dbf8ce3e915e5a1b3b7a817785a85de
Mohammed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2024-11-03 12:09:49 +0300

    my late commits

--- translate.py
+++ translate.py
@@ -173,7 +173,7 @@ def translate():
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMTnlA4E6Zr9/6b0GqAdVIl0QvZb0e6a3ANV3L5" crossorigin="anonymous">
             
             <div>
-                <i class="fas fa-copy copy-icon" role="button" style="font-size: 24px; cursor: pointer;"></i>
+                <i class="fas fa-copy copy-icon" role="button" style="font-size: 22px; cursor: pointer;"></i>
                 <span class="toastbox" role="alert"></span>
             </div>
 
